a7b70a0769f9d1d0fefa35913e290bef113471d4
Iman Mokhtari Aski <imanskater@outlook.com>
2023-06-23 14:50:58 +0330

    Added Header And IconButton Component

---
+++ src/components/IconButton.jsx
@@ -0,0 +1,14 @@
+const IconButton = (props) => {
+  const { variant, children, ...restProps } = props;
+  return (
+    <button
+      {...restProps}
+      type="button"
+      className="px-2 py-2 rounded-xl bg-slate-50/20 text-gray-300 cursor-pointer hover:bg-slate-50/30"
+    >
+      {children}
+    </button>
+  );
+};
+
+export default IconButton;
