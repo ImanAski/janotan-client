751ed8ddf9187866d2c20e8045bb92b946e493f3
Iman Mokhtari Aski <imanskater@outlook.com>
2023-06-25 03:42:26 +0330

    Temp Fix for Vertical Divider

--- src/components/Academy.jsx
+++ src/components/Academy.jsx
@@ -11,13 +11,20 @@ function Academy() {
     >
       <div className="py-24 flex-col flex items-center justify-center  ">
         <div className=" flex flex-col items-center justify-evenly  max-w-md  ">
-          <div className="inline-block h-[80px] min-h-[1em] w-0.5  bg-gray-400 opacity-100 self-center rounded-full"></div>
+          <div className="w-full flex">
+            <div className="flex-1 self-center justify-end flex"></div>
+            <div className="flex m-5 items-center justify-center">
+              <div className="inline-block h-[80px] min-h-[1em] w-[2.5px]  bg-gray-400 opacity-100 self-center rounded-full"></div>
+            </div>
+            <div className="flex-1 self-center justify-start"></div>
+          </div>
           <Typography
             className=" font-semibold tracking-wide uppercase  text-7xl text-white md:text-7xl "
             variant="h1"
           >
             academy
           </Typography>
+          {/* It seems that the divider should wrapped in Grid Or List-Style Components for functionality */}
           {/* <Divider
             className=" text-white text-7xl h-full "
             orientation="vertical"
@@ -28,10 +35,16 @@ function Academy() {
           </Divider> */}
         </div>
 
-        <div className="p-5 flex items-center justify-evenly max-w-md  w-full  ">
+        <div className="w-full flex">
+          <div className="flex-1 self-center justify-end flex">
           <Button>دپارتمان تحصیلی</Button>
-          <div className="inline-block h-[80px] min-h-[1em] w-[2.5px] self-stretch bg-gray-400 opacity-100 rounded-full"></div>
-          <Button>آکادمی و کلاس ها</Button>
+          </div>
+          <div className="flex m-5 items-center justify-center">
+            <div className="inline-block h-[80px] min-h-[1em] w-[2.5px]  bg-gray-400 opacity-100 self-center rounded-full"></div>
+          </div>
+          <div className="flex-1 self-center justify-start">
+            <Button>آکادمی و کلاس ها</Button>
+          </div>
         </div>
       </div>
     </div>
