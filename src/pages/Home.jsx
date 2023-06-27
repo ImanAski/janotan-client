b5afd6445036016871327f9713262ddefd64fa21
Negar Parvaresh <negar.prv@gmail.com>
2023-06-27 20:53:52 +0330

    blog done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,17 @@
 import StaffCarousel from "../components/StaffCarousel";
 import Header from "../components/Header";
 import Academy from "../components/Academy";
+import Blog from "../components/Blog";
 
 function Home() {
-    return (
-        <>
-            <Header />
-            <StaffCarousel />
-            <Academy />
-        </>
-    )
+  return (
+    <>
+      <Header />
+      <StaffCarousel />
+      <Academy />
+      <Blog />
+    </>
+  );
 }
-
 
 export default Home;
