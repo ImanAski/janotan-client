94d27459aad8dda38429424d2ae7f57ce1854082
Iman Mokhtari Aski <imanskater@outlook.com>
2023-07-03 17:25:16 +0330

    Added Font and fixed it

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import "./fonts.css";
 import { StyledEngineProvider } from "@mui/styled-engine";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
